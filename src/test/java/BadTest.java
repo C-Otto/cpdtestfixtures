fbf644bc9fd9f90e8f2f0e6a1f73557f2cd3b582
Carsten Otto <carsten.otto@cognitran.com>
2020-07-14 15:46:32 +0100

    initial commit

---
+++ src/test/java/BadTest.java
@@ -0,0 +1,25 @@
+public class BadTest {
+    public void name() {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    public void name2() {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
---
+++ src/testFixtures/java/BadTestFixture.java
@@ -0,0 +1,25 @@
+public class BadTestFixture {
+    public void name() {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    public void name2() {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
